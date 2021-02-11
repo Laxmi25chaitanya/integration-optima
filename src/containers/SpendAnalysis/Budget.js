54caca71f693dca6966200e8bda7c3d31d04478d
Humnabad Srivani (Europe - iDEAS-AEM) <hu20182315@wipro.com>
2021-02-11 12:09:50 +0530

    budget changes

--- src/containers/SpendAnalysis/Budget.js
+++ src/containers/SpendAnalysis/Budget.js
@@ -1,7 +1,18 @@
-const Budget = ({dailyusage,remainingBudget,totalBudget}) => {
+import SemiCircleProgressBar from "react-progressbar-semicircle"
+
+const Budget = ({ dailyusage, remainingBudget, totalBudget }) => {
+    let average = (totalBudget - remainingBudget) / 100 
+
     return (
         <div className="budget">
-            
+            <div className="link-tag">
+                <a href="">Budget</a>
+            </div>
+            <SemiCircleProgressBar percentage={average} circleRadius={100} showPercentValue />
+            <div className="perday-cost">
+                <h2>£{dailyusage} Per day</h2>
+                <h2>£{remainingBudget} Remaining out of £{totalBudget}</h2>
+            </div>
         </div>
     )
 }
