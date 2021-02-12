3e0e7e479e340270dd54dff6239c63437e7a3dba
Hariharan Sankar (Europe - iDEAS-DX) <ha20182680@wipro.com>
2021-02-12 15:45:21 +0530

    Commit

--- src/containers/SpendAnalysis/Budget.js
+++ src/containers/SpendAnalysis/Budget.js
@@ -2,7 +2,6 @@ import SemiCircleProgressBar from "react-progressbar-semicircle"
 import BudgetAnalysis from "./BudgetAnalysis";
 import { Link } from "react-router-dom";
 
-
 const Budget = ({ dailyusage, remainingBudget, totalBudget }) => {
     let average = (remainingBudget / totalBudget) * 100;
     average = Math.ceil(average);
@@ -12,26 +11,13 @@ const Budget = ({ dailyusage, remainingBudget, totalBudget }) => {
         <>
 
             <div className="budget">
-<<<<<<< HEAD
                 <div className="link-tag">
                     <Link to='/spendanalysis/budgetanalysis' >
                         Budget
                        </Link>
-
-                </div>
-
-                <SemiCircleProgressBar percentage={average} circleRadius={100} showPercentValue />
-=======
-                    <div className="link-tag">
-                       <Link to='/spendanalysis/budgetanalysis' >
-                           Budget
-                       </Link>                          
-                      
-                    </div>
-               
+                </div>               
                 <SemiCircleProgressBar percentage={average} circleRadius={100} showPercentValue strokeWidth='25' stroke='#ffc107'
                 diameter='400' />
->>>>>>> c09c57ad85594e72eaff21a12ba5c76805cda64e
                 <div className="perday-cost">
                     <h2>£{dailyusage} Per day</h2>
                     <h2>£{remainingBudget} Remaining out of £{totalBudget}</h2>
--- src/containers/SpendAnalysis/SpendAnalysis.js
+++ src/containers/SpendAnalysis/SpendAnalysis.js
@@ -5,21 +5,12 @@ import { FaAngleLeft, FaAngleRight } from 'react-icons/fa'
 import { useState } from 'react'
 
 const SpendAnalysis = () => {
-<<<<<<< HEAD
     const months = ["JANUARY", "FEBRUARY", "MARCH", "APRIL", "MAY", "JUNE", "JULY", "AUGUST", "SEPTEMBER"
         , "OCTOBER", "NOVEMBER", "DECEMBER"];
     let d = new Date().getMonth();
     const [index, setIndex] = useState(5)
     const [month, setMonth] = useState(months[index]);
-    return (
-=======
-    const months=["JANUARY","FEBRUARY","MARCH","APRIL","MAY","JUNE","JULY","AUGUST","SEPTEMBER"
-    ,"OCTOBER","NOVEMBER","DECEMBER"];
-    let d=new Date().getMonth();
-    const [index,setIndex]=useState(d)
-    const[month,setMonth]=useState(months[index]);    
-     return (
->>>>>>> c09c57ad85594e72eaff21a12ba5c76805cda64e
+    return(
         <div className="container">
             <div className="topnav">
                 <div className="leftslider arrow" >
