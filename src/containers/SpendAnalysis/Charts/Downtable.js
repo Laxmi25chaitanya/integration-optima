7cbd34424e23cdc895c78c1a56f2330f19a6d2db
Ranjithabn17 <ranjita.n82@wipro.com>
2021-02-17 14:50:05 +0530

    added changes

--- src/containers/SpendAnalysis/Charts/Downtable.js
+++ src/containers/SpendAnalysis/Charts/Downtable.js
@@ -1,16 +1,14 @@
 import React from 'react';
+
+
+
 const Downtable = () => {
+    
     return (
-        <div className="downtab">
-            <nav className="downnav">
-                <div className="titles">
-                    <ul className="downtitles">
-                        <li>Category</li>
-                        <li>Merchanct</li>
-                        <li>Country</li>
-                    </ul>
-                </div>
-            </nav>
+        <div class="categorys">
+            <button type="button" >Category</button>
+            <button type="button" >Merchent</button>
+            <button type="button" >Country</button>
         </div>
     )
 }
