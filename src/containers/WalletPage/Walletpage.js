ee24fc48ffe713346d2319d8aafd8bce79dff85a
Sreerag-P-Mohan <77611451+Sreerag-P-Mohan@users.noreply.github.com>
2021-02-22 13:34:58 +0530

    Update Walletpage.js
    Added Code for Fetching through JSON Server

--- src/containers/WalletPage/Walletpage.js
+++ src/containers/WalletPage/Walletpage.js
@@ -1,84 +1,102 @@
 import React ,{useEffect,useState} from 'react';
 import './Wallpage.css';
-import cardInfo from './cardInfo.json';
-import hsbc from './hsbc.png';
-import halifax from './halifax.png';
-import barclays from './barclays.png';
+import LeftHandNav from '../../components/leftHandNav/leftHandNav';
+import { Component } from 'react';
 
-function WalletPage () {
+class WalletPage extends Component{
+  constructor(props) {
+    super(props);
+    this.state =
+    {
+      userdata: [],
+      debit: []
+    };
+    this.getUserData = this.getUserData.bind(this);
+    this.getDebitData = this.getDebitData.bind(this);
+  }
 
-    const [data,setData]=useState([]);
-    const getData=()=>{
-      fetch('data.json'
-      ,{
-        headers : { 
-          'Content-Type': 'application/json',
-          'Accept': 'application/json'
-         }
-      }
-      )
-        .then(function(response){
-          console.log(response)
-          return response.json();
-        })
-        .then(function(myJson) {
-          console.log(myJson);
-          setData(myJson)
-        });
-    }
-    useEffect(()=>{
-      getData()
-    },[])
+  async componentDidMount() {
+    var getUserDetails = await this.getUserData();
+    var getDebitVal = await this.getDebitData();
+    this.setState({ userdata: getUserDetails, debit: getDebitVal});
+  }
 
-    return (
-      <div className ="Main">
+  async getUserData() {
+    const response = await fetch('http://localhost:8000/userdata');
+    const json = await response.json();
+    return json;
+  }
+
+  async getDebitData() {
+    const response = await fetch('http://localhost:8000/debit');
+    const json = await response.json();
+    return json;
+  }
+ 
+  render() {
+    if (this.state.userdata[0]) {
+      const data = this.state.userdata[0];
+      let bank = [3];
+      bank=this.state.debit[0]; 
+      return (
+      <div>
+        <LeftHandNav/>
+    <div className ="Main">
     <div className="Head">
       <h5 className = "h5">My Accounts</h5>
-      <div className = "para">
-        <p className ="hp1">&nbsp;&emsp;Total Balance &emsp;&emsp; Total Accounts</p>
-        <p className ="hp2">&#163; { data.totalAvailableDebitBalance } &emsp;&emsp;&emsp;{data.noOfDebitAccounts}</p>
-        <p></p>
-      </div>
+          <div className = "para">
+            <p className ="hp1">&nbsp;&emsp;Total Balance &emsp;&emsp; Total Accounts</p>
+            <p className ="hp2">&#163; { data.totalAvailableDebitBalance } &emsp;&emsp;&emsp;{data.noOfDebitAccounts}</p>
+            <p></p>
+          </div>
     </div>
     <div className="cardInfo1">
-    <img id="myImg" src={halifax}  width="310" height="120" alt="Card1"/>
-    <p className="cp1">&emsp;{cardInfo.banks[0].bankName}</p>
-    <p className="cp2">&emsp;{cardInfo.banks[0].accounts[0].interestRate} %AER</p>
+    <img id="myImg" src={`./Images/${bank.banks[0].cardimage}`}  width="310" height="120" alt="Card1"/>
+    <p className="cp1">&emsp;{bank.banks[0].bankName}</p>
+    <p className="cp2">&emsp;{bank.banks[0].accounts[0].interestRate} %AER</p>
     <p className="cp3">Total Balance  &nbsp;&nbsp;&nbsp;&emsp;&emsp;Payment Instructions</p>
-    <p className = "cp4v1">&emsp;&#163; {cardInfo.banks[0].accounts[0].balance}  &emsp;&emsp;&emsp; &#163; {cardInfo.banks[0].accounts[0].standingInst}  </p>
+    <p className = "cp4v1">&emsp;&#163; {bank.banks[0].accounts[0].balance}  &emsp;&emsp;&emsp; &#163; {bank.banks[0].accounts[0].standingInst}  </p>
     <a  className= "Link1" href="https://">View Details</a>
     <p className="cp5">&nbsp;Minimum Balance &emsp; Unutilized Balance </p>
-    <p className= "cp6v2">&emsp;&#163; {cardInfo.banks[0].accounts[0].minBalance}&nbsp;&nbsp;&nbsp;&emsp; &#163; {cardInfo.banks[0].accounts[0].availableBalance} </p>
+    <p className= "cp6v2">&emsp;&#163; {bank.banks[0].accounts[0].minBalance}&nbsp;&nbsp;&nbsp;&emsp; &#163; {bank.banks[0].accounts[0].availableBalance} </p>
+    <button className="button">OPTIMIZE</button>
+    </div>
+    
+    <div className="cardInfo2">
+    <img id="myImg1" src={`./Images/${bank.banks[1].cardimage}`}  width="287" height="120" alt="Card2"/>
+    <p className="cp1">&emsp;{bank.banks[1].bankName}</p>
+    <p className="cp2">&emsp;{bank.banks[1].accounts[0].interestRate} %AER</p>
+    <p className="cp3">Total Balance &nbsp;&nbsp;&nbsp;&emsp;&emsp; Payment Instructions</p>
+    <p className = "cp4v1">&emsp;&#163; {bank.banks[1].accounts[0].balance}  &emsp;&emsp; &#163; {bank.banks[1].accounts[0].standingInst}  </p>
+    <a  className= "Link2" href="https://">View Details </a>
+    <p className="cp5">&nbsp;Minimum Balance &emsp; Unutilized Balance </p>
+    <p className= "cp6v2">&emsp;&#163; {bank.banks[1].accounts[0].minBalance}&nbsp;&nbsp;&nbsp;&emsp;&emsp; &#163; {bank.banks[1].accounts[0].availableBalance} </p>
     <button className="button">OPTIMIZE</button>
     </div>
 
     <div className="cardInfo2">
-    <img id="myImg1" src={hsbc}  width="287" height="120" alt="Card2"/>
-    <p className="cp1">&emsp;{cardInfo.banks[1].bankName}</p>
-    <p className="cp2">&emsp;{cardInfo.banks[1].accounts[0].interestRate} %AER</p>
+    <img id="myImg1" src={`./Images/${bank.banks[2].cardimage}`}  width="287" height="120" alt="Card2"/>
+    <p className="cp1">&emsp;{bank.banks[2].bankName}</p>
+    <p className="cp2">&emsp;{bank.banks[2].accounts[0].interestRate} %AER</p>
     <p className="cp3">Total Balance &nbsp;&nbsp;&nbsp;&emsp;&emsp; Payment Instructions</p>
-    <p className = "cp4v1">&emsp;&#163; {cardInfo.banks[1].accounts[0].balance}  &emsp;&emsp; &#163; {cardInfo.banks[1].accounts[0].standingInst}  </p>
+    <p className = "cp4v1">&emsp;&#163; {bank.banks[2].accounts[0].balance}  &emsp;&emsp; &#163; {bank.banks[2].accounts[0].standingInst}  </p>
     <a  className= "Link2" href="https://">View Details </a>
     <p className="cp5">&nbsp;Minimum Balance &emsp; Unutilized Balance </p>
-    <p className= "cp6v2">&emsp;&#163; {cardInfo.banks[1].accounts[0].minBalance}&nbsp;&nbsp;&nbsp;&emsp;&emsp; &#163; {cardInfo.banks[1].accounts[0].availableBalance} </p>
+    <p className= "cp6v2">&emsp;&#163; {bank.banks[2].accounts[0].minBalance}&nbsp;&nbsp;&nbsp;&emsp;&emsp; &#163; {bank.banks[2].accounts[0].availableBalance} </p>
     <button className="button">OPTIMIZE</button>
     </div>
 
-    <div className="cardInfo3">
-    <img id="myImg2" src={barclays}  width="287" height="120" alt="Card3"/>
-    <p className="cp1">&emsp;{cardInfo.banks[2].bankName}</p>
-    <p className="cp2">&emsp;{cardInfo.banks[2].accounts[0].interestRate} %AER</p>
-    <p className="cp3">Total Balance &nbsp;&nbsp;&nbsp;&emsp;&emsp; Payment Instructions</p>
-    <p className = "cp4v1">&emsp;&#163; {cardInfo.banks[2].accounts[0].balance}  &nbsp;&emsp;&emsp; &#163; {cardInfo.banks[2].accounts[0].standingInst}  </p>
-    <a  className= "Link3" href="https://"> View Details </a>
-    <p className="cp5">&nbsp;Minimum Balance &emsp; Unutilized Balance </p>
-    <p className= "cp6v2">&emsp;&#163; {cardInfo.banks[2].accounts[0].minBalance} &nbsp;&nbsp;&nbsp;&emsp;&emsp; &#163; {cardInfo.banks[2].accounts[0].availableBalance} </p>
-    <button className="button">OPTIMIZE</button>
-
-    
     </div>
-    </div>
-      );
+    </div>   
+  );
+  }
+  else{
+    return(
+    <div className='Else'>
+    <h1>LOADING ...</h1></div>
+    )
+  }
+}
 }
 
 export default WalletPage;
