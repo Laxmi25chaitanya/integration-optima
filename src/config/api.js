b1f5a3fac46df940609c51f72d13580bc1bd0a23
Darpan Rangari <darpan.rangari@lloydsbanking.com>
2021-02-24 16:24:35 +0000

    api added for debit credit

--- src/config/api.js
+++ src/config/api.js
@@ -2,9 +2,9 @@ const api = {
    login:"/login",
    data:"/data",
     userdata:"http://localhost:8000/userdata",
-    debit:"http://localhost:8000/debit",
-    credit:"http://localhost:8000/credit",
-    totalBalances:"/data.json",
+    debit:"http://34.105.161.153/debit-service/accounts/accountsService/debit",
+    credit:"http://34.105.161.153/credit-service/accounts/accountsService/credit",
+    totalBalances:"http://34.105.161.153/totalbalance-service/account/totalBalance",
     spendanalysis:"http://34.105.161.153/spendanalysis-service/Category"
 }
 export default api;
